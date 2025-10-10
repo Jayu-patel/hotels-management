359696bf7364a7a80733fbf4e8472c48a3735d61
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2025-10-10 17:42:57 +0530

    Delete components directory

--- components/loader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function loader() {
-    return (
-        <div className="flex justify-center items-center w-full h-full">
-          <div className="h-16 w-16 border-4 border-gray-300 border-t-gray-900 rounded-full animate-spin"></div>
-        </div>
-    )
-}
